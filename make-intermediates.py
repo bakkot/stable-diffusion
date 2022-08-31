b285bad47c483f966a22b2e5c2d6adfc7cc74f73
Kevin Gibbons <bakkot@gmail.com>
2022-08-31 08:38:59 -0700

    sigh

--- make-intermediates.py
+++ make-intermediates.py
@@ -17,7 +17,7 @@ prefix = step_writer.unique_prefix()
 def image_progress(sample, step):
     global step_index
     seed = t2i.seed
-    image = t2i.model._sample_to_image(sample)
+    image = t2i._sample_to_image(sample)
     name = f'{prefix}.{seed}.{step_index}.png'
     metadata = f'{prompt} -S{seed} [intermediate]'
     step_writer.save_image_and_prompt_to_png(image, metadata, name)
