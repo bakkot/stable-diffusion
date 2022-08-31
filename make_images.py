1f3209364c9c1d782582008ef5c416b3b94a825f
Kevin Gibbons <bakkot@gmail.com>
2022-08-30 17:00:44 -0700

    fix script

--- make_images.py
+++ make_images.py
@@ -8,4 +8,11 @@ t2i.load_model()
 
 for sampler in SAMPLER_CHOICES:
     print(sampler)
-    t2i.prompt2png(f"a warrior prince, highly detailed digital art by rossdraws --sampler={sampler} --seed=999 --steps=20 --batch_size=2", "outputs/samples")
+    t2i.prompt2png(
+        prompt=f"a warrior prince, highly detailed digital art by rossdraws",
+        outdir="outputs/samples",
+        sampler=sampler,
+        seed=99999,
+        steps=15,
+        batch_size=2
+    )
